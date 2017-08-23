6e68c6060bef9dd3befd4afe4f07b5817296f3a6
Jake Kronika <jkronika@gridlined.com>
2017-08-23 12:20:30 -0500

    providing field for extended product description

--- models.py
+++ models.py
@@ -127,6 +127,7 @@ class ProductType(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
     image = models.ImageField(upload_to=utils.product_image_path,
                               null=True, blank=True)
     types = models.ManyToManyField("ProductType")
